f85a72c3da7e12fc02d5492c0b16e43a0dfa8ce2
Ashutosh <asashutosh.2004@gmail.com>
2026-08-07 15:16:14 +0530

    Improve SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,43 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "FormTools - Free PDF & Image Tools",
+  title: {
+    default: "FormTools - Free PDF & Image Tools",
+    template: "%s | FormTools",
+  },
   description:
-    "Convert, Merge, Split, Compress PDF and Resize Images online for free.",
+    "Merge, Split, Compress PDFs and Resize Images online for free. Fast, secure and easy to use.",
+
+  keywords: [
+    "PDF Tools",
+    "Merge PDF",
+    "Split PDF",
+    "Compress PDF",
+    "Image Resizer",
+    "Online PDF Tools",
+    "FormTools",
+  ],
+
+  authors: [{ name: "Ashutosh" }],
+
+  metadataBase: new URL("https://formtools-3sjv.vercel.app"),
+
+  openGraph: {
+    title: "FormTools - Free PDF & Image Tools",
+    description:
+      "Merge, Split, Compress PDFs and Resize Images online for free.",
+    url: "https://formtools-3sjv.vercel.app",
+    siteName: "FormTools",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "FormTools - Free PDF & Image Tools",
+    description:
+      "Merge, Split, Compress PDFs and Resize Images online for free.",
+  },
+
   verification: {
     google: "YB45tQsXQ7L3RuPi3LwIkfva1BaFCXT01CEwXUUJDEw",
   },
